6ee8cf24935ed5672c0ffcef5b5bd34fc3cb0dc4
Ashmeet Singh <ashmeetsingh987@gmail.com>
2021-05-16 14:48:03 +0530

    Assignment 2

---
+++ models/promotions.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+
+//var Currency = mongoose.Types.Currency;
+var Schema = mongoose.Schema;
+
+var promoSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ""
+    },
+    price: {
+        type:Number,
+        required: true,
+        min: 0
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        required: false
+    }
+}, {
+    timestamps: true
+});
+
+var Promotions = mongoose.model('Promotion', promoSchema);
+
+module.exports = Promotions;
